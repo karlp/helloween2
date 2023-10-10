99bdda4eb3e1d00b05080deff3c7ebcf81573d4a
Karl Palsson <karlp@tweak.au>
2023-10-10 10:35:10 +0000

    encoder_portable: add and fix our events
    It works now at least, but we may still use this sort of thing at a
    higher level.

--- src/encoder_portable.py
+++ src/encoder_portable.py
@@ -18,8 +18,8 @@ class Encoder:
         self._x = pin_x()
         self._y = pin_y()
         self._pos = 0
-        self._max = 0
-        self._min = 0
+        #self._max = 0
+        #self._min = 0
         self.tmax = None
         self.tmin = None
         self.ev = asyncio.ThreadSafeFlag()
@@ -31,25 +31,30 @@ class Encoder:
             self.x_interrupt = pin_x.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.x_callback)
             self.y_interrupt = pin_y.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.y_callback)
 
-    def _common_cb(self, a, b):
-        self.forward = a ^ b
+    def _common_cb(self, a, b, c):
+        self.forward = a ^ b ^ c
         self._pos += 1 if self.forward else -1
-        self._max = max(self._pos, self._max)
-        self._min = min(self._pos, self._min)
+        #self._max = max(self._pos, self._max)
+        #self._min = min(self._pos, self._min)
         if self.tmax and self._pos > self.tmax:
             self.ev.set()
-            self._cb(self._pos)
+            self._cb(self._pos)  # FIXME -probably dont do this, it does the CB in irq context.
+            self.tmax = None
+        if self.tmin and self._pos < self.tmin:
+            self.ev.set()
+            self._cb(self._pos)  # FIXME -probably dont do this, it does the CB in irq context.
+            self.tmin = None
 
 
     def x_callback(self, pin_x):
         if (x := pin_x()) != self._x:  # Reject short pulses
             self._x = x
-            self._common_cb(x, self.pin_y())
+            self._common_cb(x, self.pin_y(), 0)
 
     def y_callback(self, pin_y):
         if (y := pin_y()) != self._y:
             self._y = y
-            self._common_cb(y, self.pin_x())
+            self._common_cb(y, self.pin_x(), 1)
 
     def position(self, value=None):
         if value is not None:
@@ -63,8 +68,17 @@ class Encoder:
 
     def wait_for(self, cb, max=None, min=None):
         self._cb = cb
-        self.ev.clear()
+        self.ev.clear()  # make self.ev a "user api"
         self.tmax = max
         self.tmin = min
 
+    def wait_rel(self, cb, delta):
+        self._cb = cb
+        self.ev.clear()  # make self.ev a "user api"
+        if delta > 0:
+            self.tmax = self._pos + delta
+            self.tmin = None
+        else:
+            self.tmax = None
+            self.tmin = self._pos + delta  # yes, delta is signed...
 
