3423e32ee09792bd5d4ef8d74584efb94566df79
Karl Palsson <karlp@tweak.au>
2023-10-13 11:25:59 +0000

    much more self contained test now

--- src/aptest.py
+++ src/aptest.py
@@ -1,19 +1,47 @@
 
 import asyncio
-import binascii
 import machine
 import network
 import time
 
-import halloween2
 import secrets
+
+class KPeopleSensor:
+    """
+    This is _meant_ to just be an asyncio task wrapper for a pin change interrupt..
+    However, we get irq events while idle, without any change on the pin (scoped even)
+    we get them at a rate of about every 100ms, or, roughly a multiple thereof
+    """
+    def __init__(self, pin):
+        self.pin = pin
+        self.found = asyncio.ThreadSafeFlag()  # From isr_rules.html....
+        self.pin.irq(self._handler, trigger=machine.Pin.IRQ_FALLING)
+
+    def _handler(self, p):
+        # I _should_ only get an IRQ when it actually falls, but have been unable to figure
+        # out why I get extra... It works just fine
+        #print("handler...", p.value())  # GAH WHY SO MUCH NOISE!
+        self.found.set()
+
+    async def task_monitor(self):
+        last = time.ticks_ms()
+        while True:
+            await self.found.wait()
+            now = time.ticks_ms()
+            print(f"del: {now - last}: DETECTOR", self.pin.value())
+            last = now
+
+    def start_aio(self):
+        asyncio.create_task(self.task_monitor())
+
 
 class Core:
     def __init__(self):
-        self.app = halloween2.KApp()
+        pin = machine.Pin.board.DETECTOR
+        pin.init(pin.IN)
+        self.ps = KPeopleSensor(pin)
 
     def do_station(self):
-        """ An alternate simple start, normally we let mqtt-as just take care of bizness"""
         self.sta = network.WLAN(network.STA_IF)
         self.sta.active(False)  # reset interface
         self.sta.active(True)
@@ -23,14 +51,8 @@ class Core:
         while not self.sta.isconnected():
             time.sleep(1)
             att += 1
-            txt = f"Trying: {secrets.Wifi.SSID}, #{att}"
-            print(txt)
-            # Might be nice to even run a connection loop a few times,
-            # offer an AP for x minutes, but retry again every now and again?
-            # ie, offer a chance to have an AP to reconfigure, but still do the right thing if we're brought back
-            # into range of the desired place?
-        txt = f"Conn: {self.sta.ifconfig()[0]}"
-        print(txt)
+            print(f"Trying: {secrets.Wifi.SSID}, #{att}")
+        print(f"Conn: {self.sta.ifconfig()[0]}")
 
     def do_pintest(self):
         async def wot():
@@ -38,15 +60,14 @@ class Core:
                 print("bleh")
                 await asyncio.sleep_ms(2000)
         try:
-            self.app.people_sensor.start_aio()
+            self.ps.start_aio()
             asyncio.run(wot())
         finally:
             print("exploded, restarting!")
             asyncio.new_event_loop()
 
 
-
 c = Core()
-c.do_station()
+c.do_station()  # if we _DONT_ connect to wifi, it works fine?!
 print("ok, starting up now....")
 c.do_pintest()
